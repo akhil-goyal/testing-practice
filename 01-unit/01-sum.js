94649a1b505282ed9c4b40cf2bf0e63f4aaee266
Akhil Goyal <akhilgoyal@MacBook-Pro.phub.net.cable.rogers.com>
2020-11-23 12:34:51 -0500

    Integrated Unit Tests for file : sum.js

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,7 +1,27 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum() {
+
+// Declaring the function sum(). It takes
+// two arguments : firstArg & secondArg.
+function sum(firstArg, secondArg) {
+
+  // If firstArg is not received in the
+  // function as an argument, setting its
+  // value as 0.
+  if (firstArg === undefined) {
+    firstArg = 0;
+  }
+
+  // If secondArg is not received in the
+  // function as an argument, setting its
+  // value as 0.
+  if (secondArg === undefined) {
+    secondArg = 0;
+  }
+
+  // Returning the sum of firstArg & secondArg.
+  return firstArg + secondArg;
 
 }
 
@@ -14,28 +34,31 @@ try {
   // --------------------------------------------------
   // It should return the sum of two numbers.
   var result = sum(1, 1);
-  if (result !== 2) throw new Error('Expected sum(1, 1) to be 2. Received: ' + result);
-
+  if (result !== 2) throw new Error(`Expected sum(1, 1) to be 2. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = sum(1, 1, 2, 2, 3, 3);
+  if (result !== 2) throw new Error(`Expected sum(1,1,2,2,3,3) to be 2. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = sum(1);
+  if (result !== 1) throw new Error(`Expected sum(1) to be 1. Received: ${result}`);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if(result !== 0) throw new Error(`Expected sum() to be 0. Received: ${result}`);
+
 
   console.log('All tests passed successfully.');
-
-// ==================================================
-// PRINT ERRORS
-// ==================================================
+  // ==================================================
+  // PRINT ERRORS
+  // ==================================================
 } catch (e) {
   console.warn('Whoops, the following test did not pass:');
   console.error(e.message);
